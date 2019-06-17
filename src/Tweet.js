2ff8afe98befc5608b37fcd25b2600083cff7241
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2019-06-18 01:59:05 +0900

    Update Tweet component

--- src/Tweet.js
+++ src/Tweet.js
@@ -1,5 +1,19 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const Tweet = props => <div>{props.content}</div>;
+const Tweet = props => (
+  <div className="tweet">
+    <div className="icon-container">
+      <FontAwesomeIcon icon={props.icon} />
+    </div>
+    <div className="body-container">
+      <div className="status-display">
+        <span className="display-name">{props.displayName}</span>
+        <span className="account-name">@{props.accountName}</span>
+      </div>
+      <div>{props.content}</div>
+    </div>
+  </div>
+);
 
 export default Tweet;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Tweet from "./Tweet";
+import "./index.css";
+import { faCarrot, faCube } from "@fortawesome/free-solid-svg-icons";
 
 const App = () => (
   <div>
-    <Tweet content="Hello!" />
-    <Tweet content="World!" />
+    <Tweet
+      icon={faCube}
+      displayName="Daido Shota"
+      accountName="daido1976"
+      content="Hello!"
+    />
+    <Tweet
+      icon={faCarrot}
+      displayName="Yamada Taro"
+      accountName="yamada123"
+      content="World!"
+    />
   </div>
 );
 
