e616ba83fb6f392d6a80c6a869da298f344b1309
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2019-06-21 03:05:43 +0900

    Implement Timeline.js

---
+++ src/Timeline.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Tweet from "./Tweet";
+
+const Timeline = props => {
+  const tweetList = props.tweets.map(tweet => (
+    <Tweet
+      key={tweet.id}
+      icon={tweet.icon}
+      color={tweet.color}
+      displayName={tweet.displayName}
+      accountName={tweet.accountName}
+      content={tweet.content}
+    />
+  ));
+  return <div className="timeline">{tweetList}</div>;
+};
+
+export default Timeline;
--- src/index.js
+++ src/index.js
@@ -1,29 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Tweet from "./Tweet";
+import Timeline from "./Timeline";
 import "./index.css";
-import { library } from '@fortawesome/fontawesome-svg-core'
+import { library } from "@fortawesome/fontawesome-svg-core";
 import { faCarrot, faCube } from "@fortawesome/free-solid-svg-icons";
 
-library.add(faCarrot, faCube)
+library.add(faCarrot, faCube);
 
-const App = () => (
-  <div>
-    <Tweet
-      icon="cube"
-      color="skyblue"
-      displayName="Daido Shota"
-      accountName="daido1976"
-      content="Hello!"
-    />
-    <Tweet
-      icon="carrot"
-      color="orange"
-      displayName="Yamada Taro"
-      accountName="yamada123"
-      content="World!"
-    />
-  </div>
-);
+const App = () => {
+  const tweets = [
+    {
+      id: 0,
+      icon: "cube",
+      color: "skyblue",
+      displayName: "Daido Shota",
+      accountName: "daido1976",
+      content: "Hello!"
+    },
+    {
+      id: 1,
+      icon: "carrot",
+      color: "orange",
+      displayName: "Yamada Taro",
+      accountName: "yamada123",
+      content: "World!"
+    }
+  ];
+
+  return (
+    <div>
+      <Timeline tweets={tweets} />
+    </div>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById("root"));
